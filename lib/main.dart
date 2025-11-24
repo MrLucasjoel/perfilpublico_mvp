9d464fea05f23d6a786aeecd4d41014c8a9c29fd
LUCAS JOEL PFLEGER <lucas_j_pfleger@estudante.sesisenai.org.br>
2025-11-24 16:43:40 -0300

    add supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:perfilpublico/src/app/app_widget.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 //import 'package:perfilpublico/src/services/ad_service.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   //await AdService().initializeAds();
+  try {
+    await dotenv.load(fileName: ".env");
+    debugPrint("✅ .env carregado com sucesso!");
+  } catch (e) {
+    debugPrint("❌ Erro ao carregar .env: $e");
+  }
+
+  // Inicializando Supabase
+  try {
+    await Supabase.initialize(
+      url: dotenv.env['SUPABASE_URL']!,
+      anonKey: dotenv.env['SUPABASE_ANON_KEY']!,
+    );
+    debugPrint("✅ Supabase conectado com sucesso!");
+  } catch (e) {
+    debugPrint("❌ Erro ao conectar Supabase: $e");
+  }
+
   runApp(const AppWidget());
 }
